6039ff54bda09d35e5d8eec9225047211e572349
hamednas1000 <90717003+hamednas1000@users.noreply.github.com>
2023-03-01 21:03:41 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+
+function resizeVideo()
+{
+
+  let Video = document.querySelector('videoGrid');
+
+   height = Video.offsetHeight();
+  width = Video.offsetWidth();
+
+  console.log({height,width});
+
+
+
+
+
+}
